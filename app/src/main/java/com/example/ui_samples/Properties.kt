c1ba3e078c9d69cfc6688f119930eee397cf384c
鎌田峻輔 <s.kamada@kamatashuntasukunoMacBook-Pro.local>
2021-10-22 11:40:09 +0900

    prepare properties

---
+++ app/src/main/java/com/example/ui_samples/Properties.kt
@@ -0,0 +1,15 @@
+package com.example.ui_samples
+
+import android.text.Editable
+
+//class Properties {
+//    val factory: Factory
+//
+//    fun create(): Product {
+//        factory.addProperty("visible", "表示する文字", PropertyType.EDIT_TEXT, null)
+//        factory.addProperty("gravity", "テキストの寄せの方向", PropertyType.PULL_DOWN, listOf("left", "right", "center", "bottom"))
+//        //...
+//
+//        return factory.create()
+//    }
+//}
