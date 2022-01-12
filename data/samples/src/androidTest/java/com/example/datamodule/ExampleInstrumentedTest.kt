2d5f4004d2161ad13c92fd7ed169d6f8fe7f0ad3
鎌田峻輔 <s.kamada@kamatashuntasukunoMacBook-Pro.local>
2022-01-12 12:47:38 +0900

    Rename data:datamodule to data:samples

--- data/samples/src/androidTest/java/com/example/datamodule/ExampleInstrumentedTest.kt
+++ data/samples/src/androidTest/java/com/example/datamodule/ExampleInstrumentedTest.kt
@@ -20,6 +20,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.datamodule.test", appContext.packageName)
+        assertEquals("com.example.samples.test", appContext.packageName)
     }
 }
